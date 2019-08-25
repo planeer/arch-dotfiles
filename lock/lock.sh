c8b5379bea36cd5ebe28f13c0491fc0a18f10d5b
planeer <nejcplaner@gmail.com>
2019-08-25 17:16:06 +0200

    Lock changed, polybar comments removed

--- lock/lock.sh
+++ lock/lock.sh
@@ -1,6 +1,30 @@
-#!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -f -i /tmp/screen.png
-rm /tmp/screen.png
+#/bin/sh
+# Glichting screen lock script by xzvf
+
+pngfile="/tmp/sclock.png"
+bmpfile="/tmp/sclock.bmp"
+glitchedfile="/tmp/sclock_g.bmp"
+
+scrot -z $pngfile
+
+# convert to bmp and pixelate
+magick convert -rotate -90 $pngfile $bmpfile
+
+
+for a in {1,2,4,5,10}
+do
+    # Glitch it with sox FROM: https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
+    sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 90s : echo 1 1 $((a*2)) 0.1
+    # Rotate it by 90 degrees
+    magick convert -scale $((100/(a)))% -scale $((100*(a)))% -rotate 90 $glitchedfile $bmpfile
+done
+
+
+# Add lock icon, pixelate and convert back to png
+magick convert -gravity center -font "Hack-Bold-Nerd-Font-Complete-Mono" \
+    -pointsize 200 -draw "text 0,0 ''" -channel RGBA -fill '#bf616a' \
+    $bmpfile $pngfile
+
+i3lock -e -u -i $pngfile
+#feh $pngfile
+rm $pngfile $bmpfile $glitchedfile
