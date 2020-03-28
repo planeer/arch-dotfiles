0995109d558e0db96a4bc0c13e047ef43f0b7e65
Nejc Planer <nejcplaner@gmail.com>
2020-03-28 18:12:22 +0100

    display setup and programmes launched on boot

---
+++ i3/boot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set monitor configuration
+# TODO Find better solution for fix arg. This is needed because if we don't try to set monitors twice with any
+# difference in resolutions or positions they are not set correctly
+. ~/.config/i3/monitor.sh fix
+
+if [ $(hostname) = "floyd" ]; then
+	sleep 3 && . ~/.config/i3/monitor.sh
+fi
+
+# Auto start programs
+i3-msg "workspace 3; workspace 9; append_layout ~/.config/i3/workspace-9.json"
+teams &
+
+i3-msg "workspace 10; append_layout ~/.config/i3/workspace-10.json"
+slack &
+discord &
+
---
+++ i3/monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $(hostname) = "floyd" ]; then
+	fix=$1 # Needed for a bug when xrandr doesn't set monitors correctly
+
+	if [ -z "$fix" ]; then
+		DP3pos="2960x0"
+	else
+		DP3pos="2960x1"
+	fi
+
+	xrandr \
+	   	--output DP-1 --mode 1680x1050 --rate 60 --pos 1280x0 --primary \
+		--output DP-3 --mode 1280x1024 --rate 60 --pos $DP3pos \
+		--output DP-5 --mode 1280x1024 --rate 60 --pos 0x0
+fi
+
+feh --bg-fill ~/Documents/wallpaper.jpg
+
