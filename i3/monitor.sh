9ec5d052315d8573591ef6fccedfd1f3a9637a72
Nejc Planer <nejcplaner@gmail.com>
2020-11-13 17:49:31 +0100

    Monitors updated

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -4,15 +4,15 @@ if [ $(hostname) = "floyd" ]; then
 	fix=$1 # Needed for a bug when xrandr doesn't set monitors correctly
 
 	if [ -z "$fix" ]; then
-		DP3pos="2960x0"
+		DP3pos="3200x0"
 	else
-		DP3pos="2960x1"
+		DP3pos="3200x1"
 	fi
 
 	xrandr \
-	   	--output DP-1 --mode 1680x1050 --rate 60 --pos 1280x0 --primary \
-		--output DP-2 --mode 1680x1050 --rate 60 --pos $DP3pos \
-		--output DP-5 --mode 1280x1024 --rate 60 --pos 0x0
+		--output HDMI-0 --mode 1920x1080 --rate 60 --pos 1280x0 --primary \
+		--output DP-4 --mode 1680x1050 --rate 60 --pos $DP3pos \
+		--output DP-1 --mode 1280x1024 --rate 60 --pos 0x0
 fi
 
 feh --bg-fill ~/Documents/wallpaper.jpg
