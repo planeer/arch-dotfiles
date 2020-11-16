7ec2218ab457abf3430e74ffd7f8c4797120c953
Nejc Planer <nejcplaner@gmail.com>
2020-11-16 22:48:46 +0100

    polybar small fix

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -11,7 +11,7 @@ if [ $(hostname) = "floyd" ]; then
 
 	xrandr \
 		--output HDMI-0 --mode 1920x1080 --rate 60 --pos 1280x0 --primary \
-		--output DP-4 --mode 1680x1050 --rate 60 --pos $DP3pos \
+		--output DP-2 --mode 1680x1050 --rate 60 --pos $DP3pos \
 		--output DP-1 --mode 1280x1024 --rate 60 --pos 0x0
 fi
 
